4f0946e7a6e2ae8b3fb50b3a20584ff242ffe5b2
Brian La <43937402+BrianLa0616@users.noreply.github.com>
2019-05-07 13:35:59 -0700

    Created player and tile class

---
+++ Merchants/src/Player.java
@@ -0,0 +1,11 @@
+
+public class Player {
+	
+	private int id;
+	private int money;
+	Player(int id, int money) {
+		this.id = id;
+		this.money = money;
+	}
+	
+}
---
+++ Merchants/src/Tile.java
@@ -0,0 +1,9 @@
+
+public class Tile {
+	
+	private int type;
+
+	public Tile(int type) {
+		this.type = type;
+	}
+}
