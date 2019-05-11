acc4edf765eb6a67c5e3724dc3e5ac8504ab63f7
Ansen7 <atsan509@student.fuhsd.org>
2019-05-10 22:37:25 -0700

    Jvdc

--- Merchants/src/merchants/Merchant.java
+++ Merchants/src/merchants/Merchant.java
@@ -6,9 +6,9 @@ import board.Tile;
 import processing.core.PApplet;
 
 /**
- * walks around and purchases land, or auctions
+ * Represents a regular merchant
  * 
- * @author eylam
+ * @author Ansen
  *
  */
 public class Merchant {
@@ -18,9 +18,10 @@ public class Merchant {
 	private Color color;
 
 	/**
-	 * 
-	 * @param x x value
-	 * @param y y value
+	 * Constructs a new merchant at (x, y) with color c
+	 * @param x x coordinate of merchant
+	 * @param y y coordinate of merchant
+	 * @param c color of merchant
 	 */
 	public Merchant(int x, int y, Color c) {
 		this.x = x;
@@ -29,46 +30,93 @@ public class Merchant {
 		speed = 2;
 	}
 
-
+	/**
+	 * Moves the merchant
+	 *   
+	 * @param direction of movement 
+	 */
 	public void move(int direction) {
 		
 	}
 
+	/**
+	 * Determines whether the merchant is next to a certain tile
+	 * 
+	 * @param t tile position being compared to 
+	 * @return true if the merchant is adjacent next to the tile, false otherwise
+	 */
 	public boolean isAdjacent(Tile t) {
 		
 		return false;
 	}
 
+	/**
+	 * Purchases the land the merchant is currently on
+	 */
 	public void purchaseLand() {
 
 	}
 
+	/**
+	 * Proposes an auction for a certain land
+	 * 
+	 * @param amount of money bidding
+	 * @return the amount of bidded money
+	 */
 	public int proposeAuction(int amount) {
 		return amount;
 	}
-
+	
+	/**
+	 * Teleports the merchant to a checkpoint
+	 * 
+	 * @param id of checkpoint
+	 */
 	public void goToCheckpt(int id) {
 
 	}
 	
+	/**
+	 * Draws a new merchant
+	 * 
+	 * @param p Marker to draw things
+	 */
 	public void draw(PApplet p)
 	{
 		p.fill(color.getRed(), color.getGreen(), color.getBlue());
 		p.rect((x+0.25f)*Tile.TILE_SIZE, (y+0.25f)*Tile.TILE_SIZE, 0.5f*Tile.TILE_SIZE, 0.5f*Tile.TILE_SIZE);
 	}
 	
+	/**
+	 * 
+	 * @return x coordinate of merchant
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * 
+	 * @return y coordinate of merchant
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Sets the x coordinate of merchant
+	 * 
+	 * @param x coordinate of merchant
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * Sets the y coordinate of merchant
+	 * 
+	 * @param y coordinate of merchant
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
