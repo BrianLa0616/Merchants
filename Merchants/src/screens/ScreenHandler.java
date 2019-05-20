55021f3746c7877165e2ed70e9d73fcc3821790e
Ansen7 <atsan509@student.fuhsd.org>
2019-05-20 13:05:33 -0700

    Javadoc Screen Handler

--- Merchants/src/screens/ScreenHandler.java
+++ Merchants/src/screens/ScreenHandler.java
@@ -9,6 +9,12 @@ import other.Auction;
 import other.Player1;
 import processing.core.PApplet;
 
+/**
+ * Represents a Screen Handler which draws the screens
+ * 
+ * @author Eylam
+ *
+ */
 public class ScreenHandler extends PApplet {
 	public static final Color[] PLAYER_COLORS = { new Color(200, 0, 0), new Color(0, 0, 200), new Color(200, 200, 50),
 			new Color(50, 255, 50) };
@@ -22,6 +28,10 @@ public class ScreenHandler extends PApplet {
 	int currPlayer;
 	private Board1 board;
 
+	/**
+	 * Creates a new ScreenHandler with the current screen as the start menu screen,
+	 * and initializes the board tiles
+	 */
 	public ScreenHandler() {
 		currScreen = new IntroScreen(this);
 		players = new ArrayList<Player1>();
@@ -29,7 +39,7 @@ public class ScreenHandler extends PApplet {
 		tiles = new Tile1[15][15];
 		for (int i = 0; i < 15; i++) {
 			for (int j = 0; j < 15; j++) {
-				tiles[i][j] = new Tile1(i, j, 15 + (int)(Math.random()*10));
+				tiles[i][j] = new Tile1(i, j, 15 + (int) (Math.random() * 10));
 			}
 		}
 
@@ -37,27 +47,52 @@ public class ScreenHandler extends PApplet {
 		board = new Board1(this);
 	}
 
+	/**
+	 * Draws the screen
+	 */
 	public void draw() {
 		background(255);
 		currScreen.draw(this);
 	}
 
+	/**
+	 * Moves on to the next screen specified
+	 * 
+	 * @param s Screen desired to proceed to
+	 */
 	public void proceed(Screen s) {
 		currScreen = s;
 	}
 
+	/**
+	 * 
+	 * @return players
+	 */
 	public ArrayList<Player1> getPlayers() {
 		return players;
 	}
 
+	/**
+	 * 
+	 * @return board
+	 */
 	public Board1 getBoard() {
 		return board;
 	}
-	
+
+	/**
+	 * 
+	 * @return tiles in the board
+	 */
 	public Tile1[][] getTiles() {
 		return tiles;
 	}
-	
+
+	/**
+	 * Adds a new auction
+	 * 
+	 * @param a Auction desired to add
+	 */
 	public void addAuction(Auction a) {
 		for (int i = 0; i < auctions.size(); i++) {
 			if (a.getTile() == auctions.get(i).getTile()) {
@@ -65,17 +100,27 @@ public class ScreenHandler extends PApplet {
 				return;
 			}
 		}
-		
+
 		auctions.add(a);
 	}
-	
+
+	/**
+	 * 
+	 * @return currPlayer current player 
+	 */
 	public int getCurrentPlayer() {
 		return currPlayer;
 	}
 
+	/**
+	 * 
+	 * @return auctions that are currently happening
+	 */
 	public ArrayList<Auction> getAuction() {
 		return auctions;
 	}
+
+	
 	public void mousePressed() {
 		currScreen.mousePressed(this);
 	}
