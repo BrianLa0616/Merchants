155d38cbc119ced8c339b43104e54bdaf16b7de5
Ansen7 <atsan509@student.fuhsd.org>
2019-05-24 22:27:48 -0700

    Javadocs

--- Merchants/src/other/Bid.java
+++ Merchants/src/other/Bid.java
@@ -1,22 +1,47 @@
 package other;
 
+/**
+ * Represents a bid in an auction
+ * 
+ * @author Eylam
+ *
+ */
 public class Bid {
 	private Player player;
 	private int bid;
 
+	/**
+	 * New bid made by player
+	 * 
+	 * @param p Player making bid
+	 * @param bid amount put into bid
+	 */
 	public Bid(Player p, int bid) {
 		player = p;
 		this.bid = bid;
 	}
 
+	/**
+	 * 
+	 * @return player making bid
+	 */
 	public Player getPlayer() {
 		return player;
 	}
 
+	/**
+	 * 
+	 * @return bid amount
+	 */
 	public int getAmount() {
 		return bid;
 	}
 	
+	/**
+	 * Set bid amount
+	 * 
+	 * @param val amount set into bid
+	 */
 	public void setAmount(int val) {
 		bid = val;
 	}
